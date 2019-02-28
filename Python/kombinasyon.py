aaf8aa881f14c44b626e21354ba46bd138ef8783
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2019-02-28 13:30:57 +0300

    kombinasyon işlemi yapıldı

---
+++ Python/kombinasyon.py
@@ -0,0 +1,13 @@
+def faktoriyel(sayi):
+    sonuc = 1
+    for i in range(1, sayi + 1):
+        sonuc *= i
+    return sonuc
+
+n = int(input("n sayısını giriniz:"))
+r = int(input("r sayısını giriniz:"))
+
+
+kombinasyon = faktoriyel(n) / (faktoriyel(r) * faktoriyel(n - r))
+
+print("Kombinasyon sonucu = {}".format(kombinasyon))
